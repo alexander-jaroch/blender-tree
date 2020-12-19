d2086da6529e920cf7663a5b3ef179c301b6bb54
Alexander Jaroch <alexander-jaroch@live.com>
2020-12-19 16:24:28 +0100

    changes

--- treeTrunk.py
+++ treeTrunk.py
@@ -1,7 +1,8 @@
 import bpy
 import math
 import bmesh
-from random import randint
+from random import randint, random
+from mathutils import Vector, Matrix
 
 col = bpy.data.collections.get("Collection")    # get collection from scene hirarchy
 
@@ -24,8 +25,31 @@ radius_reduction = 0.995
 height = 5
 height_segments = height * 32
 segments = 16
-branch_height = 20
-branch_count = 20
+branch_height = 10
+branch_count = 100
+
+total_segments = segments * height_segments
+
+def get_index(n, i):
+    operations_border = [0, -segments + 1, segments, 1]
+    operations_normal = [0, 1, segments, segments + 1]
+
+    if n % segments == segments - 1:
+        return n + operations_border[i]
+    else:
+        return n + operations_normal[i]
+    
+def avg_normal(normals):
+    avg = Vector((0, 0, 0))
+    for normal in normals:
+        avg = avg + normal
+    return avg / len(normals)
+
+def get_normals(faces):
+    normals = []
+    for face in faces:
+        normals.append(face.normal)
+    return normals
 
 # calculate angle delta
 delta = (2 * math.pi) / segments
@@ -58,33 +82,55 @@ for n in range(height_segments + 1):
 # create branches
 used_faces = [False] * len(side_faces)
 
-operations_border = [0, -segments + 1, segments, 1]
-operations_normal = [0, 1, segments, segments + 1]
-
 c = 0
 
 for i in range(branch_count):
-    extrude_faces = []
     l = len(side_faces)
     r = randint(branch_height * segments, l - segments - 2)
     used = False
-    
-    for k in range(4):
-        if r % segments == 0:
-            used = used or used_faces[r + operations_border[k]]
-        else:
-            used = used or used_faces[r + operations_normal[k]]
+             
+    extrude_faces = []
         
-    if not used:
+    for k in range(4):
+        index = get_index(r, k)
+        
+        if used_faces[index]:
+            used = True
+            break
+        
+    if not used:   
         for k in range(4):
-            if r % segments == segments - 1:
-                face = side_faces[r + operations_border[k]]
-                used_faces[r + operations_border[k]] = True
-            else:
-                face = side_faces[r + operations_normal[k]]        
-                used_faces[r + operations_border[k]] = True    
-            extrude_faces.append(face)
-            bm.faces.get(face).select_set(True)
+            index = get_index(r, k)
+            
+            face = side_faces[index]
+            used_faces[index] = True
+                
+            bmface = bm.faces.get(face)            
+            extrude_faces.append(bmface)
+            bmface.select_set(True)
+            
+        branch_extrude = 5
+        for x in range(branch_extrude):
+                        
+            extruded = bmesh.ops.extrude_face_region(bm, geom=extrude_faces)            
+            translate_verts = [v for v in extruded['geom'] if isinstance(v, bmesh.types.BMVert)] 
+
+            length = (1 - r / total_segments) * 2 / branch_extrude
+            
+            random_t = (random() * 0.05 - 0.025, random() * 0.05 - 0.025, random() * 0.1)
+            
+            direction = avg_normal(get_normals(extrude_faces)) * -1 * length + Vector(random_t)
+            
+            bmesh.ops.translate(bm, vec=direction, verts=translate_verts)  
+            
+            #m = Matrix.Translation((0, 0, 0)) * Matrix.Scale(0.7, 4) * Matrix.Translation(direction)
+            #bmesh.ops.translate(bm, verts=translate_verts, space=m)  
+            #bmesh.ops.scale(bm, verts=translate_verts, space=m)            
+            
+            bmesh.ops.delete(bm, geom=extrude_faces, context="FACES")
+            
+            extrude_faces = [f for f in extruded['geom'] if isinstance(f, bmesh.types.BMFace)] 
+        
         c = c + 1
         
 print("extruded " + str(c) + " faces")
