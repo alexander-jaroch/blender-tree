d59c378d7c869c04cab8466f8ac5b9f025dbf1e7
Vivian Wolf <vivian.wolf@outlook.de>
2020-12-22 15:49:46 +0100

    Scale Faces from branches

--- treeTrunk.py
+++ treeTrunk.py
@@ -22,11 +22,11 @@ bm = bmesh.new()
 radius_bottom = 0.25
 radius_top = 0.05
 radius_reduction = 0.995
-height = 5
+height = 3
 height_segments = height * 32
 segments = 16
 branch_height = 10
-branch_count = 100
+branch_count = 50
 
 total_segments = segments * height_segments
 
@@ -113,6 +113,7 @@ for i in range(branch_count):
                         
             extruded = bmesh.ops.extrude_face_region(bm, geom=extrude_faces)            
             translate_verts = [v for v in extruded['geom'] if isinstance(v, bmesh.types.BMVert)] 
+           # print(str(translate_verts))
 
             length = (1 - r / total_segments) * 2 / branch_extrude
             
@@ -120,22 +121,29 @@ for i in range(branch_count):
             
             direction = avg_normal(get_normals(extrude_faces)) * -1 * length + Vector(random_t)
             
-            bmesh.ops.translate(bm, vec=direction, verts=translate_verts)      
-            
+            bmesh.ops.translate(bm, vec=direction, verts=translate_verts)
+            #m = Matrix.Translation((0, 0, 0)) * Matrix.Scale(0.7, 4) * Matrix.Translation(direction)
+            #bmesh.ops.translate(bm, verts=translate_verts, space=m)  
+            #bmesh.ops.scale(bm, verts=translate_verts, space=m) 
+             
             bmesh.ops.delete(bm, geom=extrude_faces, context="FACES")
             
-            extrude_faces = [f for f in extruded['geom'] if isinstance(f, bmesh.types.BMFace)] 
-            
+            extrude_faces = [f for f in extruded['geom'] if isinstance(f, bmesh.types.BMFace)]
             for face in extrude_faces:
-                face.select_set(True)                
-                
-            # Irgendwas machen
+                for m in range(len(extrude_faces)):
+                    face.select_set(True)
+                    calculation = extrude_faces[m].calc_center_median()
+                    vertices = extrude_faces[m].verts
+                    translation_matrix = Matrix.Translation(-calculation)
+                    scale = Matrix.Scale(0.8,4)
+                    bmesh.ops.transform(bm, matrix=scale, verts=vertices, space=translation_matrix)
+
                 
             edges = [e for e in extruded['geom'] if isinstance(e, bmesh.types.BMEdge)]
             
             for edge in edges:
-                edge.select_set(False) 
-        
+                edge.select_set(False)
+            
         c = c + 1
         
 print("extruded " + str(c) + " faces")
