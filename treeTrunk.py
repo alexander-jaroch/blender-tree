8a874cc01eb00db206bdf229fbf5c066f1ee954e
Alexander Jaroch <alexander-jaroch@live.com>
2020-12-23 11:14:50 +0100

    added test.blend, added .gitignore

--- treeTrunk.py
+++ treeTrunk.py
@@ -112,8 +112,7 @@ for i in range(branch_count):
         for x in range(branch_extrude):
                         
             extruded = bmesh.ops.extrude_face_region(bm, geom=extrude_faces)            
-            translate_verts = [v for v in extruded['geom'] if isinstance(v, bmesh.types.BMVert)] 
-           # print(str(translate_verts))
+            translate_verts = [v for v in extruded['geom'] if isinstance(v, bmesh.types.BMVert)]
 
             length = (1 - r / total_segments) * 2 / branch_extrude
             
@@ -122,27 +121,35 @@ for i in range(branch_count):
             direction = avg_normal(get_normals(extrude_faces)) * -1 * length + Vector(random_t)
             
             bmesh.ops.translate(bm, vec=direction, verts=translate_verts)
-            #m = Matrix.Translation((0, 0, 0)) * Matrix.Scale(0.7, 4) * Matrix.Translation(direction)
-            #bmesh.ops.translate(bm, verts=translate_verts, space=m)  
-            #bmesh.ops.scale(bm, verts=translate_verts, space=m) 
              
             bmesh.ops.delete(bm, geom=extrude_faces, context="FACES")
             
             extrude_faces = [f for f in extruded['geom'] if isinstance(f, bmesh.types.BMFace)]
+            
+            center = Vector((0,0,0))
+            vertcount = 0
+            face_verts = set()
             for face in extrude_faces:
-                for m in range(len(extrude_faces)):
-                    face.select_set(True)
-                    calculation = extrude_faces[m].calc_center_median()
-                    vertices = extrude_faces[m].verts
-                    translation_matrix = Matrix.Translation(-calculation)
-                    scale = Matrix.Scale(0.8,4)
-                    bmesh.ops.transform(bm, matrix=scale, verts=vertices, space=translation_matrix)
+                for vert in face.verts:           
+                    center = center + vert.co
+                    vertcount = vertcount + 1
+                    face_verts.add(vert)
+            center = center / vertcount       
+            
+            for face in extrude_faces:
+                #face.select_set(True)
+                #calculation = face.calc_center_median()
+                #calculation = center
+                #vertices = face.verts
+                translation_matrix = Matrix.Translation(-center)
+                scale = Matrix.Scale(0.91,4)
+                bmesh.ops.transform(bm, matrix=scale, verts=list(face_verts), space=translation_matrix)
 
                 
-            edges = [e for e in extruded['geom'] if isinstance(e, bmesh.types.BMEdge)]
+            #edges = [e for e in extruded['geom'] if isinstance(e, bmesh.types.BMEdge)]
             
-            for edge in edges:
-                edge.select_set(False)
+            #for edge in edges:
+            #    edge.select_set(False)
             
         c = c + 1
         
